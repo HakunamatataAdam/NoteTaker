6fb63ebe0f6f308de398b02e0c2a8a38d9103c96
HakunamatatatatatatatataAdam <19315@burnside.school.nz>
2023-10-23 15:38:20 +1300

    final

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-
-#app
+import datetime
+# app
 app = Flask(__name__)
 
 DATABASE = 'notetaker.db'
 
-app.config['SECRET_KEY'] ='uisrth4et485tw6t76t7yf'
+app.config['SECRET_KEY'] = 'uisrth4et485tw6t76t7yf'
 
 
 def get_db():
@@ -23,8 +23,7 @@ def get_db():
     return db
 
 
-
-def query_db(sql,args=(),one=False):
+def query_db(sql, args=(), one=False):
     '''connect and query- will retun one item if one=true and can accept arguments as tuple'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -34,6 +33,7 @@ def query_db(sql,args=(),one=False):
     db.close()
     return (results[0] if results else None) if one else results
 
+
 @app.route('/')
 def index():
     conn = sqlite3.connect("notetaker.db")
@@ -41,56 +41,56 @@ def index():
     cur.execute("SELECT * FROM note")
     results = cur.fetchall()
     print(results)
-    conn.close()
-    
+    conn.close()   
+
     return render_template("index.html", results=results)
 
-#passes an id to this route- we then query for THIS item and send just that 
-#data to a template.
-@app.route('/login', methods=["GET","POST"])
+# passes an id to this route- we then query for THIS item and send just that 
+# data to a template.
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    #if the user posts a username and password
+    # if the user posts a username and password
     if request.method == "POST":
-        #get the username and password
+        # get the username and password
         username = request.form['username']
         password = request.form['password']
-        #try to find this user in the database- note- just keepin' it simple so usernames must be unique
+        # try to find this user in the database- note- just keepin' it simple so usernames must be unique
         sql = "SELECT * FROM user WHERE username = ?"
-        user = query_db(sql=sql,args=(username,),one=True)
+        user = query_db(sql=sql, args=(username,), one=True)
         if user:
-            #check password matches-
-            if check_password_hash(user[2],password):
-                #Store the username in the session
+            # check password matches-
+            if check_password_hash(user[2], password):
+                # Store the username in the session
                 session['user'] = user
                 flash("Logged in successfully")
             else:
                 flash("Password incorrect")
         else:
             flash("Username does not exist")
-    #render this template regardles of get/post
+    # render this template regardles of get/post
     return render_template('login.html')
 
 
-@app.route('/signup', methods=["GET","POST"])
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
-    #if the user posts from the signup page
+    # if the user posts from the signup page
     if request.method == "POST":
-        #add the new username and hashed password to the database
+        # add the new username and hashed password to the database
         username = request.form['username']
         password = request.form['password']
-        #hash it with the cool secutiry function
+        # hash it with the cool secutiry function
         hashed_password = generate_password_hash(password)
-        #write it as a new user to the database
+        # write it as a new user to the database
         sql = "INSERT INTO user (username,password) VALUES (?,?)"
-        query_db(sql,(username,hashed_password))
-        #message flashes exist in the base.html template and give user feedback
+        query_db(sql, (username, hashed_password))
+        # message flashes exist in the base.html template and give user feedback
         flash("Sign Up Successful")
     return render_template('register.html')
 
 
 @app.route('/logout')
 def logout():
-    #clears the username from the session and redirect back to the home page
+    # clears the username from the session and redirect back to the home page
     session['user'] = None
     return redirect('/')
 
@@ -101,9 +101,9 @@ def create_note():
         cursor = get_db().cursor()
         title = request.form["title"]
         body = request.form["body"]
-        time = request.form["time"]
-        sql = "INSERT INTO note(title, body, time) VALUES (?,?,?)"
-        cursor.execute(sql,(title, body, time))
+        user = session["user"]
+        sql = "INSERT INTO note(title, body, time, user_id) VALUES (?,?,?,?)"
+        cursor.execute(sql, (title, body, datetime.datetime.now(), user[0]))
         get_db().commit()
     return redirect('/')
 
@@ -130,13 +130,13 @@ def create_note():
 
 #     return render_template('note.html', post=post)
 
-@app.route('/delete', methods=["GET","POST"])
+@app.route('/delete', methods=["GET", "POST"])
 def delete():
     id = request.form['id']
-    db= get_db()
+    db = get_db()
     cursor = db.cursor()
     sql = "DELETE FROM note WHERE id = ?"
-    cursor.execute(sql,(id,))
+    cursor.execute(sql, (id,))
     db.commit()
 
     return redirect(url_for('index'))
@@ -145,8 +145,8 @@ def delete():
 def note(id):
     conn = sqlite3.connect('notetaker.db')
     cur = conn.cursor()
-    result=cur.execute(
-        """ SELECT title, body, time FROM note WHERE id = ?;""",(id,)
+    result = cur.execute(
+        """ SELECT id, title, body, time FROM note WHERE id = ?;""",(id,)
     ).fetchone()
     print(result)
 
@@ -154,14 +154,14 @@ def note(id):
 
 @app.route('/edit', methods=["GET","POST"])
 def edit():
-    body =request.form.get('body')
-    id =request.form.get('id')
-    print(body,id)
+    body = request.form.get('body')
+    id = request.form.get('id')
+    print(body, id)
     conn = sqlite3.connect('notetaker.db')
     cur = conn.cursor()
-    result=cur.execute('UPDATE note SET body = ? WHERE id=?',(body,id))
+    result = cur.execute('UPDATE note SET body = ? WHERE id=?', (body, id))
     conn.commit()
-    return redirect(url_for('note',id=id))
+    return redirect(url_for('note', id=id))
 
 
 @app.errorhandler(404)
@@ -171,4 +171,3 @@ def error(e):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
